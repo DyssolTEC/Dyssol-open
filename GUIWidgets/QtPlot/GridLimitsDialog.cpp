c3e9c86f89da7abf43563cf21a06fe5b5f552456
Anton Gladky <Anton.Gladky@iam.tu-freiberg.de>
2021-01-16 11:31:03 +0100

    Fix crash

--- GUIWidgets/QtPlot/GridLimitsDialog.cpp
+++ GUIWidgets/QtPlot/GridLimitsDialog.cpp
@@ -8,7 +8,7 @@ CGridLimitsDialog::CGridLimitsDialog(QWidget *parent)
 	ui.setupUi(this);
 
 	m_dXMin = m_dXMax = m_dYMin = m_dYMax = NULL;
-	*m_bLogX = *m_bLogY = false;
+	m_bLogX = m_bLogY = false;
 
 	QObject::connect( ui.buttonOk, SIGNAL(clicked()), this, SLOT(accept()) );
 	QObject::connect( ui.buttonCancel, SIGNAL(clicked()), this, SLOT(close()) );
@@ -28,10 +28,10 @@ void CGridLimitsDialog::SetLabels( QString _sXLabel, QString _sYLabel )
 
 void CGridLimitsDialog::SetLimits( double& _dXMin, double& _dXMax, double& _dYMin, double& _dYmax )
 {
-	m_dXMin = &_dXMin;
-	m_dXMax = &_dXMax;
-	m_dYMin = &_dYMin;
-	m_dYMax = &_dYmax;
+	m_dXMin = _dXMin;
+	m_dXMax = _dXMax;
+	m_dYMin = _dYMin;
+	m_dYMax = _dYmax;
 
 	UpdateWholeView();
 }
@@ -40,14 +40,14 @@ void CGridLimitsDialog::UpdateLogScaling(void)
 {
 	if ((m_bLogX == NULL) || (m_bLogY == NULL))
 		return;
-	*m_bLogX = ui.checkLogX->isChecked();
-	*m_bLogY = ui.checkLogY->isChecked();
+	m_bLogX = ui.checkLogX->isChecked();
+	m_bLogY = ui.checkLogY->isChecked();
 }
 
 void CGridLimitsDialog::SetLogScaling(bool &_bLogX, bool &_bLogY)
 {
-	m_bLogX = &_bLogX;
-	m_bLogY = &_bLogY;
+	m_bLogX = _bLogX;
+	m_bLogY = _bLogY;
 	UpdateLogScaling();
 }
 
@@ -58,19 +58,19 @@ void CGridLimitsDialog::UpdateWholeView()
 
 	UpdateLogScaling();
 
-	ui.lineEditXMin->setText( QString::number( *m_dXMin ) );
-	ui.lineEditXMax->setText( QString::number( *m_dXMax ) );
-	ui.lineEditYMin->setText( QString::number( *m_dYMin ) );
-	ui.lineEditYMax->setText( QString::number( *m_dYMax ) );
+	ui.lineEditXMin->setText( QString::number( m_dXMin ) );
+	ui.lineEditXMax->setText( QString::number( m_dXMax ) );
+	ui.lineEditYMin->setText( QString::number( m_dYMin ) );
+	ui.lineEditYMax->setText( QString::number( m_dYMax ) );
 }
 
 void CGridLimitsDialog::ApplyChanges()
 {
-	*m_dXMin = ui.lineEditXMin->text().toDouble();
-	*m_dXMax = ui.lineEditXMax->text().toDouble();
+	m_dXMin = ui.lineEditXMin->text().toDouble();
+	m_dXMax = ui.lineEditXMax->text().toDouble();
 
-	*m_dYMin = ui.lineEditYMin->text().toDouble();
-	*m_dYMax = ui.lineEditYMax->text().toDouble();
+	m_dYMin = ui.lineEditYMin->text().toDouble();
+	m_dYMax = ui.lineEditYMax->text().toDouble();
 
 	UpdateLogScaling();
 }
