d67bd4cb3107fd038356d2c2f01d45b001d746c4
Vasyl Skorych <j.CherOks@gmail.com>
2021-02-09 09:22:32 +0100

    refactor: Use new API in Agglomerator, AgglomeratorLitster

--- Units/Agglomerator/Agglomerator.h
+++ Units/Agglomerator/Agglomerator.h
@@ -4,15 +4,13 @@
 
 #include "DynamicUnit.h"
 #include "DAESolver.h"
-#include "MaterialStream.h"
-#include "DistributionsFunctions.h"
-#include <algorithm>
+#include "Stream.h"
 
 class CUnitDAEModel : public CDAEModel
 {
 public:
 	/// Indexes of state variables for solver ///
-	unsigned m_nq0;		// PSD
+	size_t m_nq0{};		// PSD
 
 	std::vector<double> m_vBRate, m_vDRate; // Variables to store current birth- and death-rate
 
@@ -24,19 +22,19 @@ public:
 class CAgglomerator : public CDynamicUnit
 {
 private:
-	CUnitDAEModel m_Model;
-	CDAESolver m_Solver;
+	CUnitDAEModel m_Model{};
+	CDAESolver m_Solver{};
 
 public:
-	CAgglomerationSolver *m_pAggSolver;	// External agglomeration calculator
+	CAgglomerationSolver *m_pAggSolver{};	// External agglomeration calculator
 
-	CHoldup* m_pHoldup;					// Internal holdup
-	CMaterialStream* m_pInStream;		// Inlet
-	CMaterialStream* m_pOutStream;		// Outlet
+	CHoldup* m_pHoldup{};					// Internal holdup
+	CMaterialStream* m_pInStream{};			// Inlet
+	CMaterialStream* m_pOutStream{};		// Outlet
 
-	unsigned m_nClassesNum;				// Number of classes for PSD
-	std::vector<double> m_vSizeGrid;	// Size grid for PSD
-	std::vector<double> m_vSizes;		// Class sizes for PSD
+	size_t m_nClassesNum{};					// Number of classes for PSD
+	std::vector<double> m_vSizeGrid;		// Size grid for PSD
+	std::vector<double> m_vSizes;			// Class sizes for PSD
 
 public:
 	void CreateBasicInfo() override;
