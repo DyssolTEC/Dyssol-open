482246f0ddb55763a03ab106c80994f6c93a4d9e
Vasyl Skorych <j.CherOks@gmail.com>
2021-07-07 18:23:06 +0200

    chore: Add missing copyright statement

--- SimulatorCore/LogUpdater.h
+++ SimulatorCore/LogUpdater.h
@@ -1,3 +1,5 @@
+/* Copyright (c) 2021, Dyssol Development Team. All rights reserved. This file is part of Dyssol. See LICENSE file for license information. */
+
 #pragma once
 
 #include "BaseUnit.h"
