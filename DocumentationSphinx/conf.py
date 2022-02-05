af150a550a0f3889c28271bff0d31ca1a8f8bace
Vasyl Skorych <j.CherOks@gmail.com>
2022-02-05 20:01:07 +0100

    docs: Update sphinx view configuration.
    Change theme to sphinx-rtd.
    Set icon.
    Update other settings.
    Remove build docs to latex.

--- DocumentationSphinx/conf.py
+++ DocumentationSphinx/conf.py
@@ -19,14 +19,14 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'Dyssol Help'
-copyright = '2019-2020, Xiye Zhou'
+project = 'Dyssol'
+copyright = '2022, Dyssol Development Team'
 author = 'Dyssol Development Team'
 
 # The short X.Y version
-version = ''
+version = 'latest'
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = 'latest'
 
 
 # -- General configuration ---------------------------------------------------
@@ -40,8 +40,10 @@ release = '0.0.1'
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinx.ext.mathjax',
+    'sphinx.ext.duration',
     'sphinx.ext.githubpages',
+    'sphinx.ext.mathjax',
+    'sphinx_rtd_theme',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -77,17 +79,26 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'classic'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
 html_theme_options = {
-	"stickysidebar": "true",
-	"sidebarwidth": "300px",
-	"body_max_width": "1300px"
+    'logo_only': True,
+    'display_version': False,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': True,
+    'vcs_pageview_mode': 'blob',
+    'style_nav_header_background': '#0E70B6',
+    'collapse_navigation': False,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': False,
+    'titles_only': False,    
 }
+github_url = 'https://github.com/FlowsheetSimulation/Dyssol-open'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -110,76 +121,7 @@ html_static_path = ['pics']
 # Output file base name for HTML help builder.
 htmlhelp_basename = 'Dyssol'
 
-
-# -- Options for LaTeX output ------------------------------------------------
-
-latex_elements = {
-    # The paper size ('letterpaper' or 'a4paper').
-    #
-    # 'papersize': 'letterpaper',
-
-    # The font size ('10pt', '11pt' or '12pt').
-    #
-    # 'pointsize': '10pt',
-
-    # Additional stuff for the LaTeX preamble.
-    #
-    # 'preamble': '',
-
-    # Latex figure (float) alignment
-    #
-    # 'figure_align': 'htbp',
-}
-
-# Grouping the document tree into LaTeX files. List of tuples
-# (source start file, target name, title,
-#  author, documentclass [howto, manual, or own class]).
-latex_documents = [
-    (master_doc, 'DyssolHelpDoc.tex', 'Dyssol Documentation',
-     'Xiye Zhou', 'manual'),
-]
-
-
-# -- Options for manual page output ------------------------------------------
-
-# One entry per manual page. List of tuples
-# (source start file, name, description, authors, manual section).
-man_pages = [
-    (master_doc, 'dyssol', 'Dyssol Documentation',
-     [author], 1)
-]
-
-
-# -- Options for Texinfo output ----------------------------------------------
-
-# Grouping the document tree into Texinfo files. List of tuples
-# (source start file, target name, title, author,
-#  dir menu entry, description, category)
-texinfo_documents = [
-    (master_doc, 'Dyssol', 'Dyssol Documentation',
-     author, 'Dyssol authors', 'Dyssol - dynamic flowsheet simulator.',
-     'Miscellaneous'),
-]
-
-
-# -- Options for Epub output -------------------------------------------------
-
-# Bibliographic Dublin Core info.
-epub_title = project
-
-# The unique identifier of the text. This can be a ISBN number
-# or the project homepage.
-#
-# epub_identifier = ''
-
-# A unique identification for the text.
-#
-# epub_uid = ''
-
-# A list of files that should not be packed into the epub file.
-epub_exclude_files = ['search.html']
-
-html_logo = './pics/logo.png'
-
+html_logo = './pics/logo_doc.png'
+html_favicon = './pics/logo.png'
 
 # -- Extension configuration -------------------------------------------------
