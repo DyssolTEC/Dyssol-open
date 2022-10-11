8a43285f0f77fd8a051d77e2e620f108f8d627ba
Vasyl Skorych <J.CherOks@gmail.com>
2022-10-11 17:06:56 +0200

    refactor: Cyclone2
    Fix calculation of d_star - Cut size for separation in internal vortex

--- Units/Cyclone2/Cyclone2.h
+++ Units/Cyclone2/Cyclone2.h
@@ -6,40 +6,45 @@
 
 class CCyclone2 : public CSteadyStateUnit
 {
-	/// Cyclone parameters
+	// Cyclone parameters
+
 	// User input
-	double m_d_r_out{ 0 };		// Outer radius of cyclone [m]
-	double m_d_r_dip{ 0 };		// Radius of dip tube [m]
-	double m_d_h_dip{ 0 };		// Height of dip tube [m]
-	double m_d_b_e{ 0 };		// Width of rectangular entry [m]
-	double m_d_h_e{ 0 };		// Height of rectangular entry [m]
-	double m_d_lambda_0{ 0 };	// Friction coefficient [m]
-	double m_d_h_tot{ 0 };		// Total height of cyclone [m]
-	double m_d_h_cyl{ 0 };		// Height of cylindrical part of cyclone [m]
-	double m_d_r_ex{ 0 };		// Radius of particle exit [m]
-	double m_n_D{ 0 };
-	double m_d_eta_sep{ 0 };	// Additional adjustment of separation [-]
+	double r_a{};		// Outer radius of cyclone [m].
+	double r_i{};		// Radius of the dip tube [m].
+	double h_dip{};		// Height of the dip tube [m].
+	double b{};			// Width of rectangular entry [m].
+	double h_e{};		// Height of rectangular entry [m].
+	double lambda_0{};	// Wall friction coefficient for pure gas [-].
+	double h_tot{};		// Total height of cyclone [m].
+	double h_cyl{};		// Height of the cylindrical part of the cyclone [m].
+	double r_3{};		// Radius of the particle exit [m].
+	double D_init{};	// Initial value for calculation of separation efficiency curve [-].
+	double K_G{};		// Constant for solids loading threshold [-].
+	double eta_sep{};	// Additional efficiency factor of separation [-].
 	// Calculated
-	double m_d_r_mean{ 0 };		// Mean radius [m]
-	double m_d_r_cone{ 0 };		// Mean cone radius [m]
-	double m_d_r_e{ 0 };		// Center stream line of entry [m]
-	double m_d_h_cone{ 0 };		// Height of the cone [m]
-	double m_d_h_inner{ 0 };	// Total height minus depth of dip tube [m]
-	double m_d_beta{ 0 };		// Geometry factor [-]
-	double m_d_A_first{ 0 };	// Area considered for first round after entry [m^2]
-	double m_d_A_cyl{ 0 };		// Lateral area cylinder [m^2]
-	double m_d_A_cone{ 0 };		// Complete cone area [m^2]
-	double m_d_A_dip{ 0 };		// Lateral area dip tube [m^2]
-	double m_d_A_top{ 0 };		// Area upper wall [m^2]
-	double m_d_A_fric{ 0 };		// Total friction area [m^2]
-	double m_d_Ahalf_cone{ 0 };	// Area at half of conical part [m^2]
-	double m_d_A_sed{ 0 };		// Sedimentation area [m^2]
-	/// Simulation parameters
-	bool m_b_calc_D{ false };			// Variable to control calculation of D
-	bool m_b_plot{ false };				// Variable to control plotting functions
-	size_t m_n_num_size_classes{ 0 };	// Number of classes of particle size distribution [-]
-	std::vector<double> m_v_size_grid;	// Vector with bins of PSD
-	std::vector<double> m_v_dpart_grid; // Vector with mean diameters of PSD
+	double r_2{};		// Mean cone radius [m].
+	double r_e{};		// Distance to the middle streamline of entry [m].
+	double h_con{};		// Height of the cone part of the cyclone [m].
+	double h_i{};		// Height of separation zone [m].
+	double beta{};		// Geometry factor [-].
+	double A_e1{};		// Wall area considered on average for the first round after entry [m^2].
+	double A_cyl{};		// Lateral area of the cylindrical part of the wall [m^2].
+	double A_con{};		// Lateral area of the conical part of the wall [m^2].
+	double A_dip{};		// Lateral area of the dip tube [m^2].
+	double A_top{};		// Area of the upper wall [m^2].
+	double A_R{};		// Total wall friction area [m^2].
+	double A_con_2{};	// Lateral area of the top half of the conical part of the wall [m^2].
+	double A_w{};		// Sedimentation area [m^2].
+
+	// Simulation parameters
+
+	bool plot{ false };			// Flag to control plotting functions.
+
+	// Flowsheet parameters
+
+	size_t classesNum{};			// Number of PSD classes [#].
+	std::vector<double> averDiam;	// Average values of size grid for PSD [m].
+	std::vector<double> sizeGrid;	// Vector with bins of PSD
 
 public:
 	void CreateBasicInfo() override;
@@ -48,12 +53,14 @@ public:
 	void Simulate(double _time) override;
 
 private:
-	/// Helper functions
-	double CalculateDeff(double _d_dpart_star);
-	void CalculateSeparationMuschelknauz(double _time, CMaterialStream* _inflow, CMaterialStream* _outflowCoarse, CMaterialStream* _outflowFines);
-
-	/// Check functions
+	// Check input parameters.
 	bool CheckCycloneParameters() const;
-	void CheckStream(double _time, CMaterialStream* _stream);
+	// Main calculation function.
+	void CalculateSeparationMuschelknauz(double _time, CStream* _inlet, CStream* _outletC, CStream* _outletF);
+	// Calculation effective value of D parameter according to Muschelknautz.
+	double CalculateDeff(double d_star, double D_init) const;
+	// Calculate separation efficiency in internal vortex for particle of size d.
+	double CalculateSeparationFun(double d_star, double D, double d) const;
+
 };
 
