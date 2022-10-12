be95542b3fab95b7373bfd183635c721e56e8ff6
Vasyl Skorych <J.CherOks@gmail.com>
2022-10-12 16:35:53 +0200

    fix: GranulatorSimpleBatch
    Fix, cleanup and refactor

--- Units/GranulatorSimpleBatch/GranulatorSimpleBatch.h
+++ Units/GranulatorSimpleBatch/GranulatorSimpleBatch.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2020, Dyssol Development Team. All rights reserved. This file is part of Dyssol. See LICENSE file for license information. */
+/* Copyright (c) 2022, Dyssol Development Team. All rights reserved. This file is part of Dyssol. See LICENSE file for license information. */
 
 #pragma once
 
@@ -8,36 +8,34 @@ class CUnitDAEModel : public CDAEModel
 {
 public:
 	/// Indices of state variables for solver ///
-	size_t m_iAtot{};	// Total surface of all particles in the Granulator
-	size_t m_iMtot{};	// Total mass of all particles in the Granulator
-	size_t m_iMFlowExhaustGas{};	// Output exhaust gas
-	size_t m_iG{};		// Growth rate
-	size_t m_iMtotq3{}; // Product of Mtot and q3
+	size_t m_iAtot{};		// Total surface of all particles in the Granulator.
+	size_t m_iMtot{};		// Total mass of all particles in the Granulator.
+	size_t m_iMFlowExh{};	// Mass flow of output exhaust gas.
+	size_t m_iG{};			// Growth rate.
+	size_t m_iMtotq3{};		// Product of Mtot and q3
 
 public:
 	void CalculateResiduals(double _time, double* _vars, double* _ders, double* _res, void* _unit) override;
 	void ResultsHandler(double _time, double* _vars, double* _ders, void* _unit) override;
 };
 
-class CMyGranulatorWithoutThermo : public CDynamicUnit
+class CGranulatorSimpleBatch : public CDynamicUnit
 {
 private:
 	CUnitDAEModel m_model;
 	CDAESolver m_solver;
 
 public:
-	CHoldup* m_holdup{};				// Holdup
-	CMaterialStream* m_inSuspStream{};	// Input of solution
-	CMaterialStream* m_inGasStream{};	// Input gas stream
-	CMaterialStream* m_outExhaustGasStream{};	// Output of solids
+	CHoldup* m_holdup{};						// Holdup.
+	CMaterialStream* m_inSuspStream{};			// Input of solution.
+	CMaterialStream* m_inGasStream{};			// Input gas stream.
+	CMaterialStream* m_outExhaustGasStream{};	// Output of solids.
 
-	size_t m_classesNum{};				// Number of classes for PSD
-	std::vector<double> m_sizeGrid;		// Size grid for PSD
-	std::vector<double> m_avgDiam;		// Average values of size grid for PSD
-	std::vector<double> m_classSize;	// Class sizes of size grid for PSD
-	double m_initMass{};				// Initial mass in the Granulator
-	std::vector<double> m_diamRatio;	// Vector: stores ratio of two adjacent diameter values, for calculating G
-	std::vector<double> m_massFrac;		// Mass fraction of classes
+	size_t m_classesNum{};				// Number of classes for PSD.
+	std::vector<double> m_sizeGrid;		// Size grid for PSD.
+	std::vector<double> m_avgDiam;		// Average values of size grid for PSD.
+	std::vector<double> m_classSize;	// Class sizes of size grid for PSD.
+	std::vector<double> m_diamRatio;	// Vector: stores ratio of two adjacent diameter values, for calculating G.
 
 public:
 	void CreateBasicInfo() override;
