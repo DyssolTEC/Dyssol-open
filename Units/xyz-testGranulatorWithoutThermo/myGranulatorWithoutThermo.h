e70c33debed7bc092060b038f6aaf99a3d07b5f1
Vasyl Skorych <J.CherOks@gmail.com>
2022-10-12 11:08:19 +0200

    feat: Add MyGranulatorWithoutThermo from Xiye

---
+++ Units/xyz-testGranulatorWithoutThermo/myGranulatorWithoutThermo.h
@@ -0,0 +1,49 @@
+/* Copyright (c) 2020, Dyssol Development Team. All rights reserved. This file is part of Dyssol. See LICENSE file for license information. */
+
+#pragma once
+
+#include "UnitDevelopmentDefines.h"
+
+class CUnitDAEModel : public CDAEModel
+{
+public:
+	/// Indices of state variables for solver ///
+	size_t m_iAtot{};	// Total surface of all particles in the Granulator
+	size_t m_iMtot{};	// Total mass of all particles in the Granulator
+	size_t m_iMFlowExhaustGas{};	// Output exhaust gas
+	size_t m_iG{};		// Growth rate
+	size_t m_iMtotq3{}; // Product of Mtot and q3 
+
+public:
+	void CalculateResiduals(double _time, double* _vars, double* _ders, double* _res, void* _unit) override;
+	void ResultsHandler(double _time, double* _vars, double* _ders, void* _unit) override;
+};
+
+class CMyGranulatorWithoutThermo : public CDynamicUnit
+{
+private:
+	CUnitDAEModel m_model;
+	CDAESolver m_solver;
+
+public:
+	CHoldup* m_holdup{};				// Holdup
+	CMaterialStream* m_inSuspStream{};	// Input of solution
+	CMaterialStream* m_inGasStream{};	// Input gas stream
+	CMaterialStream* m_outExhaustGasStream{};	// Output of solids
+
+	size_t m_classesNum{};				// Number of classes for PSD
+	std::vector<double> m_sizeGrid;		// Size grid for PSD
+	std::vector<double> m_avgDiam;		// Average values of size grid for PSD
+	std::vector<double> m_classSize;	// Class sizes of size grid for PSD
+	double m_initMass{};				// Initial mass in the Granulator
+	std::vector<double> m_diamRatio;	// Vector: stores ratio of two adjacent diameter values, for calculating G
+	std::vector<double> m_massFrac;		// Mass fraction of classes
+
+public:
+	void CreateBasicInfo() override;
+	void CreateStructure() override;
+	void Initialize(double _time) override;
+	void SaveState() override;
+	void LoadState() override;
+	void Simulate(double _timeBeg, double _timeEnd) override;
+};
