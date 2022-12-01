3c80c8ac26c71efc018acd6a6ebf760261f5b999
Vasyl Skorych <J.CherOks@gmail.com>
2022-12-01 20:22:44 +0100

    build: Allow compilation with SUNDIALS 6

--- ExternalLibraries/CompileSundials.ps1
+++ ExternalLibraries/CompileSundials.ps1
@@ -12,7 +12,13 @@ if (-not (Get-Command Expand-7Zip -ErrorAction Ignore)) {
 ################################################################################
 ### Paths
 
-$SUNDIALS_VERSION = "5.6.1"
+$SUNDIALS_MAJOR_VERSION = "6"
+$SUNDIALS_MIDDLE_VERSION = "4"
+$SUNDIALS_MINOR_VERSION = "1"
+#$SUNDIALS_MAJOR_VERSION = "5"
+#$SUNDIALS_MIDDLE_VERSION = "6"
+#$SUNDIALS_MINOR_VERSION = "1"
+$SUNDIALS_VERSION = "$SUNDIALS_MAJOR_VERSION.$SUNDIALS_MIDDLE_VERSION.$SUNDIALS_MINOR_VERSION"
 $SUNDIALS_DOWNLOAD_ADDRESS = "https://github.com/LLNL/sundials/releases/download/v$SUNDIALS_VERSION/sundials-$SUNDIALS_VERSION.tar.gz"
 $SUNDIALS_NAME = "sundials-$SUNDIALS_VERSION"
 $SUNDIALS_TAR_NAME = "$SUNDIALS_NAME.tar"
@@ -59,10 +65,17 @@ Rename-Item -Path "$SUNDIALS_INSTALL_PATH\lib" -NewName "$SUNDIALS_INSTALL_PATH\
 ### Copy additional files
 
 # Copy header files
-Copy-Item "$SUNDIALS_SRC_PATH\src\ida\ida_impl.h" "$SUNDIALS_INSTALL_PATH\include\ida\ida_impl.h"
-Copy-Item "$SUNDIALS_SRC_PATH\src\ida\ida_ls_impl.h" "$SUNDIALS_INSTALL_PATH\include\ida\ida_ls_impl.h"
-Copy-Item "$SUNDIALS_SRC_PATH\src\kinsol\kinsol_impl.h" "$SUNDIALS_INSTALL_PATH\include\kinsol\kinsol_impl.h"
-Copy-Item "$SUNDIALS_SRC_PATH\src\kinsol\kinsol_ls_impl.h" "$SUNDIALS_INSTALL_PATH\include\kinsol\kinsol_ls_impl.h"
+if ([int]$SUNDIALS_MAJOR_VERSION -eq 5) {
+	Copy-Item "$SUNDIALS_SRC_PATH\src\ida\ida_impl.h" "$SUNDIALS_INSTALL_PATH\include\ida\ida_impl.h"
+	Copy-Item "$SUNDIALS_SRC_PATH\src\ida\ida_ls_impl.h" "$SUNDIALS_INSTALL_PATH\include\ida\ida_ls_impl.h"
+}
+if ([int]$SUNDIALS_MAJOR_VERSION -eq 6) {
+	Copy-Item "$SUNDIALS_SRC_PATH\src\ida\ida_impl.h" "$SUNDIALS_INSTALL_PATH\include\ida\ida_impl.h"
+	Copy-Item "$SUNDIALS_SRC_PATH\src\ida\ida_ls_impl.h" "$SUNDIALS_INSTALL_PATH\include\ida\ida_ls_impl.h"
+	Copy-Item "$SUNDIALS_SRC_PATH\src\sundials\sundials_context_impl.h" "$SUNDIALS_INSTALL_PATH\include\sundials_context_impl.h"
+	Copy-Item "$SUNDIALS_SRC_PATH\src\sundials\sundials_hashmap.h" "$SUNDIALS_INSTALL_PATH\include\sundials_hashmap.h"
+	Copy-Item "$SUNDIALS_SRC_PATH\src\sundials\sundials_logger_impl.h" "$SUNDIALS_INSTALL_PATH\include\sundials_logger_impl.h"
+}
 
 # Copy *.pdb files
 Copy-Item "$SUNDIALS_BUILD_PATH\x32\src\ida\sundials_ida_obj_static.dir\Debug\sundials_ida_obj_static.pdb" "$SUNDIALS_INSTALL_PATH\lib32\sundials_ida_obj_static.pdb"
