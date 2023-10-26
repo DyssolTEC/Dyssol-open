1ccd7dc900c9cf82425712d846a7e3f25f0aef7b
ytykhonova <julia2000tikhonova@gmail.com>
2023-10-26 21:57:40 +0200

    docs(CStream): Add doc for functions

--- ModelsAPI/Stream.h
+++ ModelsAPI/Stream.h
@@ -6,7 +6,10 @@
 
 class CHoldup;
 
-/* Class describes the material flow. */
+/**
+* \brief Class describes the material flow.
+* \details
+*/
 class CStream : public CBaseStream
 {
 	friend CHoldup;
@@ -21,55 +24,171 @@ public:
 	// Copy constructor.
 	CStream(const CBaseStream& _other);
 
-	// Copies all data at the given time point from another stream. All data after the time point are removed from this stream.
+	/**
+	* \brief Copies all data at the given time point from another stream.
+	* \details All data after the time point are removed from this stream. Refer to function CBaseStream::Copy(double, const CBaseStream&).
+	* \param _time Target time point to copy.
+	* \param _source Source stream.
+	*/
 	void CopyFromStream(double _time, const CStream* _source);
-	// Copies all data at the given time interval from another stream. All data after the end time point are removed from this stream.
+	/**
+	* \brief Copies all data at the given time interval from another stream.
+	* \details All data after the end time point are removed from this stream. Refer to function CBaseStream::Copy(double, double, const CBaseStream&).
+	* \param _timeBeg Begin of the time interval to copy.
+	* \param _timeEnd End of the time interval to copy.
+	* \param _source Source stream.
+	*/
 	void CopyFromStream(double _timeBeg, double _timeEnd, const CStream* _source);
-	// Copies all data from the given time point of another stream to another time point of this stream. All data after the time point are removed from this stream.
+	/**
+	* \brief Copies all data from the given time point of another stream to another time point of this stream.
+	* \details All data after the time point are removed from this stream. Refer to function CBaseStream::Copy(double, const CBaseStream&, double).
+	* \param _timeDst Time point of the destination stream to copy.
+	* \param _source Source stream.
+	* \param _timeSrc Time point of the source stream to copy.
+	*/
 	void CopyFromStream(double _timeDst, const CStream* _source, double _timeSrc);
 
-	// Copies all data at the given time point from the holdup. All data after the time point are removed from this stream.
+	/**
+	* \brief Copies all data at the given time point from the holdup.
+	* \details All data after the time point are removed from this stream.
+	* \param _time Target time point to copy.
+	* \param _source Source holdup.
+	* \param _massFlow Value of the overall mass flow.
+	*/
 	void CopyFromHoldup(double _time, const CHoldup* _source, double _massFlow);
-	// Copies all data from the given time point of the holdup to another time point of this stream. All data after the time point are removed from this stream.
+	/**
+	* \brief Copies all data from the given time point of the holdup to another time point of this stream.
+	* \details All data after the time point are removed from this stream.
+	* \param _timeDst Time point of the destination stream to copy.
+	* \param _source Source holdup.
+	* \param _timeSrc Time point of the source holdup to copy.
+	* \param _massFlow Value of the overall mass flow.
+	*/
 	void CopyFromHoldup(double _timeDst, const CHoldup* _source, double _timeSrc, double _massFlow);
 
-	// Mixes the specified stream with the current stream at the given time point.
+	/**
+	* \brief Mixes the specified stream with the current stream at the given time point.
+	* \details Refer to function CBaseStream::Add(double, const CBaseStream&).
+	* \param _time Target time point to copy.
+	* \param _source Source stream.
+	*/
 	void AddStream(double _time, const CStream* _source);
-	// Mixes the specified stream with the current stream for each time point from the given time interval.
+	/**
+	* \brief Mixes the specified stream with the current stream for each time point from the given time interval.
+	* \details Refer to function CBaseStream::Add(double, double, const CBaseStream&).
+	* \param _timeBeg Begin of the time interval.
+	* \param _timeEnd End of the time interval.
+	* \param _source Source stream.
+	*/
 	void AddStream(double _timeBeg, double _timeEnd, const CStream* _source);
 
-	// Returns the mass flow of the stream at the given time point.
+	/**
+	* \brief Returns the mass flow of the stream in [kg/s] at the given time point.
+	* \details Refer to CBaseStream::GetMass(double) const.
+	* \param _time Target time point.
+	* \return Mass flow.
+	*/
 	double GetMassFlow(double _time) const;
-	// Returns the molar flow of the stream at the given time point.
+	/**
+	* \brief Returns the molar flow of the stream in [mol/s] at the given time point.
+	* \details Refer to function CBaseStream::GetMol(double) const.
+	* \param _time Target time point.
+	* \return Molar flow.
+	*/
 	double GetMolFlow(double _time) const;
-	// Sets the mass flow of the stream at the given time point.
+	/**
+	* \brief Sets the mass flow of the stream in [kg/s] at the given time point.
+	* \details Refer to function CBaseStream::SetMass(double, double).
+	* \param _time Target time point.
+	* \param _value Value of the mass flow.
+	*/
 	void SetMassFlow(double _time, double _value);
-	// Sets the molar flow of the stream at the given time point.
+	/**
+	* \brief Sets the molar flow of the stream in [mol/s] at the given time point.
+	* \details Refer to function CBaseStream::SetMol(double, double).
+	* \param _time Target time point.
+	* \param _value Value of the molar flow.
+	*/
 	void SetMolFlow(double _time, double _value);
 
-	// Returns the mass flow of the compound in the specified phase at the given time point.
+	/**
+	* \brief Returns the mass flow of the compound in the specified phase at the given time point.
+	* \details Refer to function CBaseStream::GetCompoundMass(double, const std::string&, EPhase) const.
+	* \param _time Target time point.
+	* \param _compoundKey Unique key of the compound.
+	* \param _phase Identifier of phase type.
+	* \return Mass flow of the compound.
+	*/
 	double GetCompoundMassFlow(double _time, const std::string& _compoundKey, EPhase _phase) const;
-	// Returns the mass flow of the compound in the total mixture at the given time point.
+	/**
+	* \brief Returns the mass flow of the compound in the total mixture at the given time point.
+	* \details Refer to function CBaseStream::GetCompoundMass(double, const std::string&) const.
+	* \param _time Target time point.
+	* \param _compoundKey Unique key of the compound.
+	* \return Mass flow of the compound.
+	*/
 	double GetCompoundMassFlow(double _time, const std::string& _compoundKey) const;
-	// Returns the molar flow of the compound in the specified phase at the given time point.
+	/**
+	* \brief Returns the molar flow of the compound in the specified phase at the given time point.
+	* \details Refer to function CBaseStream::GetCompoundMol(double, const std::string&, EPhase) const.
+	* \param _time Target time point.
+	* \param _compoundKey Unique key of the compound.
+	* \param _phase Identifier of phase type.
+	* \return Molar flow of the compound.
+	*/
 	double GetCompoundMolFlow(double _time, const std::string& _compoundKey, EPhase _phase) const;
-	// Returns mass flows of all defined compounds at the given time point.
+	/**
+	* \brief Returns mass flows of all defined compounds at the given time point.
+	* \details Refer to function CBaseStream::GetCompoundsMasses(double) const.
+	* \param _time Target time point.
+	* \return Mass flows of all defined compounds.
+	*/
 	std::vector<double> GetCompoundsMassFlows(double _time) const;
-	// Returns mass flows of all defined compounds in the specified phase at the given time point.
+	/**
+	* \brief Returns mass flows of all defined compounds in the specified phase at the given time point.
+	* \details Refer to function CBaseStream::GetCompoundsMasses(double, EPhase) const.
+	* \param _time Target time point.
+	* \param _phase Identifier of phase type.
+	* \return Mass flows of all defined compounds.
+	*/
 	std::vector<double> GetCompoundsMassFlows(double _time, EPhase _phase) const;
 
-	// Returns the mass flow of the specified phase at the given time point.
+	/**
+	* \brief Returns the mass flow of the specified phase at the given time point.
+	* \details Refer to function CBaseStream::GetPhaseMass(double, EPhase) const.
+	* \param _time Target time point.
+	* \param _phase Indentifier of phase type.
+	* \return Mass flow of the specified phase.
+	*/
 	double GetPhaseMassFlow(double _time, EPhase _phase) const;
-	// Returns the molar flow of the specified phase at the given time point.
+	/**
+	* \brief Returns the molar flow of the specified phase at the given time point.
+	* \details Refer to function CBaseStream::GetPhaseMol(double, EPhase) const.
+	* \param _time Target time point.
+	* \param _phase Indentifier of phase type.
+	* \return Molar flow of the specified phase.
+	*/
 	double GetPhaseMolFlow(double _time, EPhase _phase) const;
-	// Sets the mass flow of the specified phase at the given time point. Total mass flow of the stream is correspondingly adjusted, masses of other phases remain the same.
+	/**
+	* \brief Sets the mass flow of the specified phase at the given time point.
+	* \details Total mass flow of the stream is correspondingly adjusted, masses of other phases remain the same. Refer to function CBaseStream::SetPhaseMass(double, EPhase, double).
+	* \param _time Target time point.
+	* \param _phase Indentifier of phase type.
+	* \param _value Value of mass flow.
+	*/
 	void SetPhaseMassFlow(double _time, EPhase _phase, double _value);
-	// Sets the molar flow of the specified phase at the given time point. Total mass flow of the stream is correspondingly adjusted, masses of other phases remain the same.
+	/**
+	* \brief Sets the molar flow of the specified phase at the given time point.
+	* \details Total mass flow of the stream is correspondingly adjusted, masses of other phases remain the same. Refer to function CBaseStream::SetPhaseMol(double, EPhase, double).
+	* \param _time Target time point.
+	* \param _phase Indentifier of phase type.
+	* \param _value Molar flow of the specified phase.
+	*/
 	void SetPhaseMolFlow(double _time, EPhase _phase, double _value);
 
 	// TODO: move it somewhere
 	////////////////////////////////////////////////////////////////////////////////
-	/// Deprecated functions
+	// Deprecated functions
 	[[deprecated("WARNING! CopyFromStream(const CStream*, double) is deprecated. Use CopyFromStream(double, const CStream*) instead.")]]
 	void CopyFromStream(const CStream* _source, double _time, bool _deleteDataAfter = true);
 	[[deprecated("WARNING! CopyFromStream(const CStream*, double, double) is deprecated. Use CopyFromStream(double, double, const CStream*) instead.")]]
