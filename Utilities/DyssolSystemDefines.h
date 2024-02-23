f74222947fc951f479eee9356155792740d23b6c
Vasyl Skorych <vasyl.skorych@gmail.com>
2024-02-23 13:45:37 +0100

    Version 1.3.0

--- Utilities/DyssolSystemDefines.h
+++ Utilities/DyssolSystemDefines.h
@@ -1,4 +1,6 @@
-/* Copyright (c) 2020, Dyssol Development Team. All rights reserved. This file is part of Dyssol. See LICENSE file for license information. */
+/* Copyright (c) 2020, Dyssol Development Team.
+ * Copyright (c) 2024, DyssolTEC GmbH.
+ * All rights reserved. This file is part of Dyssol. See LICENSE file for license information. */
 
 #pragma once
 
@@ -6,7 +8,7 @@
 #include "DyssolHelperDefines.h"
 
 #define VERSION_0			1
-#define VERSION_1			2
+#define VERSION_1			3
 #define VERSION_2			0
 #define CURRENT_VERSION		VERSION_0,VERSION_1,VERSION_2
 #define CURRENT_VERSION_STR	MACRO_TOSTRING(VERSION_0) "." MACRO_TOSTRING(VERSION_1) "." MACRO_TOSTRING(VERSION_2)
