d6198ef0e8181498a4540d63fd771e50a7a9a7e8
Vasyl Skorych <vasyl.skorych@gmail.com>
2024-02-23 12:46:16 +0100

    build: More excludes in copy_to_home.sh

--- scripts/copy_to_home.sh
+++ scripts/copy_to_home.sh
@@ -4,4 +4,4 @@
 # Copyright (c) 2023, DyssolTEC GmbH. All rights reserved. 
 # This file is part of Dyssol. See LICENSE file for license information.
 
-rsync -av --exclude=.git --exclude=.vs --exclude=build --exclude=DyssolInstallers/Compiler --exclude=DyssolInstallers/Installers --exclude=ExternalLibraries --exclude=install --exclude=Win32 --exclude=x64 /mnt/d/Codes/dyssol ~/
+rsync -av --exclude=.git --exclude=.vs --exclude=build --exclude=Documentation/sources --exclude=DyssolConsole/cache --exclude=DyssolInstallers/Compiler --exclude=DyssolInstallers/Installers --exclude=ExternalLibraries --exclude=install --exclude=Win32 --exclude=x64 /mnt/d/Codes/dyssol ~/
