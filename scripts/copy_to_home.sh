3528a989eaa72bda8f755a7251dc7c8032bbc4f2
Vasyl Skorych <vasyl.skorych@gmail.com>
2024-07-09 17:28:19 +0200

    chore: Update script for WSL build

--- scripts/copy_to_home.sh
+++ scripts/copy_to_home.sh
@@ -4,4 +4,9 @@
 # Copyright (c) 2023, DyssolTEC GmbH. All rights reserved. 
 # This file is part of Dyssol. See LICENSE file for license information.
 
-rsync -av --exclude=.git --exclude=.vs --exclude=build --exclude=Documentation/sources --exclude=DyssolConsole/cache --exclude=DyssolInstallers/Compiler --exclude=DyssolInstallers/Installers --exclude=ExternalLibraries --exclude=install --exclude=Win32 --exclude=x64 /mnt/d/Codes/dyssol ~/
+# Absolute path to this script, e.g. /home/user/bin/script.sh
+SCRIPT=$(readlink -f "$0")
+# Absolute path this script is in, thus /home/user/bin
+SCRIPTPATH=$(dirname "$SCRIPT")
+# Run copy
+rsync -av --exclude=.git --exclude=.vs --exclude=build --exclude=Documentation/sources --exclude=DyssolConsole/cache --exclude=DyssolInstallers/Compiler --exclude=DyssolInstallers/Installers --exclude=ExternalLibraries --exclude=install --exclude=Win32 --exclude=x64 ${SCRIPTPATH}/../ ~/dyssol/
