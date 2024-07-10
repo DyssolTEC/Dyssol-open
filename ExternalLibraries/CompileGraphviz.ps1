b5e800ae2a38422ad8a0a92696ba99eba0160902
Vasyl Skorych <vasyl.skorych@gmail.com>
2024-07-10 23:35:34 +0200

    build: Remove x86 config from external libraries scripts

--- ExternalLibraries/CompileGraphviz.ps1
+++ ExternalLibraries/CompileGraphviz.ps1
@@ -1,4 +1,6 @@
-# Copyright (c) 2021, Dyssol Development Team. All rights reserved. This file is part of Dyssol. See LICENSE file for license information.
+# Copyright (c) 2021, Dyssol Development Team.
+# Copyright (c) 2024, DyssolTEC GmbH.
+# All rights reserved. This file is part of Dyssol. See LICENSE file for license information.
 
 ################################################################################
 ### Initializing
@@ -28,7 +30,7 @@ $GRAPHVIZ_INSTALL_PATH   = "$CURRENT_PATH\graphviz"
 $GRAPHVIZ_SRC_PATH       = "$CURRENT_PATH\$GRAPHVIZ_NAME"
 $GRAPHVIZ_BUILD_PATH     = "$GRAPHVIZ_SRC_PATH\build"
 
-# libs
+# Libs
 $ZLIB_INSTALL_PATH = "$CURRENT_PATH\zlib"
 
 ################################################################################
@@ -41,70 +43,24 @@ Remove-Item $GRAPHVIZ_SRC_PATH     -Force -Recurse -ErrorAction Ignore
 ################################################################################
 ### Download
 
-# clone selected version
+# Clone selected version
 git clone --branch $GRAPHVIZ_VERSION --depth 1 $GRAPHVIZ_GIT_ADDRESS $GRAPHVIZ_SRC_PATH
 
 ################################################################################
 ### Build and install
 
-# download and initialize dependencies
+# Download and initialize dependencies
 Set-Location $GRAPHVIZ_SRC_PATH
 git submodule update --init
 
-# paths to dependencies
-$DEPEND_PATH    = "$GRAPHVIZ_SRC_PATH\windows\dependencies"
-$DEPEND_UTILS   = "$DEPEND_PATH\graphviz-build-utilities"
-$DEPEND_LIBS32  = "$DEPEND_PATH\libraries\x86"
-$DEPEND_LIBS64  = "$DEPEND_PATH\libraries\x64"
-$DEPEND_VCPKG32 = "$DEPEND_PATH\libraries\vcpkg\installed\x86-windows"
-$DEPEND_VCPKG64 = "$DEPEND_PATH\libraries\vcpkg\installed\x64-windows"
+# Paths to dependencies
+$DEPEND_PATH  = "$GRAPHVIZ_SRC_PATH\windows\dependencies"
+$DEPEND_UTILS = "$DEPEND_PATH\graphviz-build-utilities"
+$DEPEND_LIBS  = "$DEPEND_PATH\libraries\x64"
 
-# Build x32
-New-Item $GRAPHVIZ_BUILD_PATH\x32 -ItemType directory
-Set-Location $GRAPHVIZ_BUILD_PATH\x32
-cmake -G "Visual Studio 16 2019" -A Win32 $GRAPHVIZ_SRC_PATH `
-	-DCMAKE_INSTALL_PREFIX:PATH=$GRAPHVIZ_INSTALL_PATH `
-	-DCMAKE_DISABLE_FIND_PACKAGE_ANN=YES `
-	-DCMAKE_DISABLE_FIND_PACKAGE_CAIRO=YES `
-	-DCMAKE_DISABLE_FIND_PACKAGE_GD=YES `
-	-DCMAKE_DISABLE_FIND_PACKAGE_GTK2=YES `
-	-DCMAKE_DISABLE_FIND_PACKAGE_Freetype=YES `
-	-DCMAKE_DISABLE_FIND_PACKAGE_NSIS=YES `
-	-DCMAKE_DISABLE_FIND_PACKAGE_PANGOCAIRO=YES `
-	-DPKG_CONFIG_EXECUTABLE="" `
-	-DBISON_EXECUTABLE="$DEPEND_UTILS\winflexbison\win_bison.exe" `
-	-DBUILD_TESTING=NO `
-	-DEXPAT_INCLUDE_DIR="$DEPEND_LIBS32\include" `
-	-DEXPAT_LIBRARY="$DEPEND_LIBS32\lib\expat.lib" `
-	-DEXPAT_RUNTIME_LIBRARIES="$DEPEND_LIBS32\bin\expat.dll" `
-	-DFLEX_EXECUTABLE="$DEPEND_UTILS\winflexbison\win_flex.exe" `
-	-DFLEX_INCLUDE_DIR="$DEPEND_UTILS\winflexbison" `
-	-DLTDL_INCLUDE_DIR="$DEPEND_LIBS32\include" `
-	-DZLIB_INCLUDE_DIR="$ZLIB_INSTALL_PATH\include" `
-	-DZLIB_LIBRARY_DEBUG="$ZLIB_INSTALL_PATH\lib32\zlibstaticd.lib" `
-	-DZLIB_LIBRARY_RELEASE="$ZLIB_INSTALL_PATH\lib32\zlibstatic.lib" `
-	-Denable_ltdl=YES `
-	-Duse_coverage=NO `
-	-Duse_sanitizers=NO `
-	-Duse_win_pre_inst_libs=YES `
-	-Dwith_cxx_api=NO `
-	-Dwith_cxx_tests=NO `
-	-Dwith_digcola=NO `
-	-Dwith_expat=YES `
-	-Dwith_gvedit=NO `
-	-Dwith_ipsepcola=NO `
-	-Dwith_ortho=NO `
-	-Dwith_sfdp=NO `
-	-Dwith_smyrna=NO `
-	-Dwith_zlib=YES
-cmake --build . --parallel --target INSTALL --config Release
-Rename-Item -Path "$GRAPHVIZ_INSTALL_PATH\bin" -NewName "$GRAPHVIZ_INSTALL_PATH\bin32"
-Rename-Item -Path "$GRAPHVIZ_INSTALL_PATH\lib" -NewName "$GRAPHVIZ_INSTALL_PATH\lib32"
-Copy-Item -Path "$CURRENT_PATH\graphviz_config" -Destination "$GRAPHVIZ_INSTALL_PATH\bin32\config6"
-
-# Build x64
-New-Item $GRAPHVIZ_BUILD_PATH\x64 -ItemType directory
-Set-Location $GRAPHVIZ_BUILD_PATH\x64
+# Build
+New-Item $GRAPHVIZ_BUILD_PATH -ItemType directory
+Set-Location $GRAPHVIZ_BUILD_PATH
 cmake -G "Visual Studio 16 2019" -A x64 $GRAPHVIZ_SRC_PATH `
 	-DCMAKE_INSTALL_PREFIX:PATH=$GRAPHVIZ_INSTALL_PATH `
 	-DCMAKE_DISABLE_FIND_PACKAGE_ANN=YES `
@@ -117,15 +73,15 @@ cmake -G "Visual Studio 16 2019" -A x64 $GRAPHVIZ_SRC_PATH `
 	-DPKG_CONFIG_EXECUTABLE="" `
 	-DBISON_EXECUTABLE="$DEPEND_UTILS\winflexbison\win_bison.exe" `
 	-DBUILD_TESTING=NO `
-	-DEXPAT_INCLUDE_DIR="$DEPEND_LIBS64\include" `
-	-DEXPAT_LIBRARY="$DEPEND_LIBS64\lib\expat.lib" `
-	-DEXPAT_RUNTIME_LIBRARIES="$DEPEND_LIBS64\bin\expat.dll" `
+	-DEXPAT_INCLUDE_DIR="$DEPEND_LIBS\include" `
+	-DEXPAT_LIBRARY="$DEPEND_LIBS\lib\expat.lib" `
+	-DEXPAT_RUNTIME_LIBRARIES="$DEPEND_LIBS\bin\expat.dll" `
 	-DFLEX_EXECUTABLE="$DEPEND_UTILS\winflexbison\win_flex.exe" `
 	-DFLEX_INCLUDE_DIR="$DEPEND_UTILS\winflexbison" `
-	-DLTDL_INCLUDE_DIR="$DEPEND_LIBS64\include" `
+	-DLTDL_INCLUDE_DIR="$DEPEND_LIBS\include" `
 	-DZLIB_INCLUDE_DIR="$ZLIB_INSTALL_PATH\include" `
-	-DZLIB_LIBRARY_DEBUG="$ZLIB_INSTALL_PATH\lib64\zlibstaticd.lib" `
-	-DZLIB_LIBRARY_RELEASE="$ZLIB_INSTALL_PATH\lib64\zlibstatic.lib" `
+	-DZLIB_LIBRARY_DEBUG="$ZLIB_INSTALL_PATH\lib\zlibstaticd.lib" `
+	-DZLIB_LIBRARY_RELEASE="$ZLIB_INSTALL_PATH\lib\zlibstatic.lib" `
 	-Denable_ltdl=YES `
 	-Duse_coverage=NO `
 	-Duse_sanitizers=NO `
@@ -141,16 +97,13 @@ cmake -G "Visual Studio 16 2019" -A x64 $GRAPHVIZ_SRC_PATH `
 	-Dwith_smyrna=NO `
 	-Dwith_zlib=YES
 cmake --build . --parallel --target INSTALL --config Release
-Rename-Item -Path "$GRAPHVIZ_INSTALL_PATH\bin" -NewName "$GRAPHVIZ_INSTALL_PATH\bin64"
-Rename-Item -Path "$GRAPHVIZ_INSTALL_PATH\lib" -NewName "$GRAPHVIZ_INSTALL_PATH\lib64"
-Copy-Item -Path "$CURRENT_PATH\graphviz_config" -Destination "$GRAPHVIZ_INSTALL_PATH\bin64\config6"
+Copy-Item -Path "$CURRENT_PATH\graphviz_config" -Destination "$GRAPHVIZ_INSTALL_PATH\bin\config6"
 
 ################################################################################
 ### Clean installation directory
 
 $REM_GRAPHVIZ_ROOT_LIST = @(
-	"lib32\pkgconfig",
-	"lib64\pkgconfig",
+	"lib\pkgconfig",
 	"share"
 )
 $REM_GRAPHVIZ_DLL_LIST = @(
@@ -204,27 +157,25 @@ $REM_GRAPHVIZ_LIB_LIST = @(
 	"xdot"
 )
 
-# gather
+# Gather
 $REM_LIST = @("TEMP_TO_DEL")
 foreach ($item in $REM_GRAPHVIZ_ROOT_LIST) {
 	$REM_LIST += $GRAPHVIZ_INSTALL_PATH + '\' + $item
 }
 foreach ($item in $REM_GRAPHVIZ_DLL_LIST) {
-	$REM_LIST += $GRAPHVIZ_INSTALL_PATH + '\bin32\' + $item + '.dll'
-	$REM_LIST += $GRAPHVIZ_INSTALL_PATH + '\bin64\' + $item + '.dll'
+	$REM_LIST += $GRAPHVIZ_INSTALL_PATH + '\bin\' + $item + '.dll'
 }
 foreach ($item in $REM_GRAPHVIZ_INCLUDE_LIST) {
 	$REM_LIST += $GRAPHVIZ_INSTALL_PATH + '\include\graphviz\' + $item + '.h'
 }
 foreach ($item in $REM_GRAPHVIZ_LIB_LIST) {
-	$REM_LIST += $GRAPHVIZ_INSTALL_PATH + '\lib32\' + $item + '.lib'
-	$REM_LIST += $GRAPHVIZ_INSTALL_PATH + '\lib64\' + $item + '.lib'
+	$REM_LIST += $GRAPHVIZ_INSTALL_PATH + '\lib\' + $item + '.lib'
 }
-# remove
+# Remove
 foreach ($item in $REM_LIST) {
 	Remove-Item "$item" -Force -Recurse -ErrorAction Ignore
 }
-# remove all binaries
+# Remove all binaries
 Remove-Item "$GRAPHVIZ_INSTALL_PATH\*" -Recurse -Include *.exe
 
 ################################################################################
