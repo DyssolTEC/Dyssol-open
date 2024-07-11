67649f8ef85f4e7a48cdfdc64cb015d8becfb1ce
Vasyl Skorych <vasyl.skorych@gmail.com>
2024-07-12 00:33:28 +0200

    build: Remove x86 from Tests project

--- DyssolInstallers/Documentation/build_docs.ps1
+++ DyssolInstallers/Documentation/build_docs.ps1
@@ -1,3 +1,6 @@
+# Copyright (c) 2024, DyssolTEC GmbH. 
+# All rights reserved. This file is part of Dyssol. See LICENSE file for license information.
+
 # read arguments
 $solution_dir = $args[0]
 
--- DyssolInstallers/Tests/Script.bat
+++ DyssolInstallers/Tests/Script.bat
@@ -1,3 +1,4 @@
-:: Copyright (c) 2023, DyssolTEC. All Rights Reserved. This file is part of Dyssol. See LICENSE file for license information.
+:: Copyright (c) 2023, DyssolTEC. 
+:: All Rights Reserved. This file is part of Dyssol. See LICENSE file for license information.
 
-powershell -executionpolicy bypass -File %~dp0\build_and_run_tests.ps1 %1 %2 %3 %4 %5
+powershell -executionpolicy bypass -File %~dp0\build_and_run_tests.ps1 %1 %2 %3 %4
