f5cd56972182b57ebe60acf8bb3a0cdbbf81f50a
Vasyl Skorych <vasyl.skorych@gmail.com>
2024-09-07 13:50:10 +0200

    build: Drop x86 support

--- ExternalLibraries/RunAll.bat
+++ ExternalLibraries/RunAll.bat
@@ -1,4 +1,6 @@
-:: Copyright (c) 2020, Dyssol Development Team. All rights reserved. This file is part of Dyssol. See LICENSE file for license information.
+:: Copyright (c) 2020, Dyssol Development Team. 
+:: Copyright (c) 2024, DyssolTEC GmbH. 
+:: All rights reserved. This file is part of Dyssol. See LICENSE file for license information.
 
 powershell -executionpolicy bypass -File .\CompileSundials.ps1
 powershell -executionpolicy bypass -File .\CompileZLib.ps1
