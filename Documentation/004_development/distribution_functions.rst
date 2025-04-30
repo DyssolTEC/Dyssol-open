37dd508b6ce7b357283931b56a421e192eb2ff48
Vasyl Skorych <vasyl.skorych@gmail.com>
2025-04-30 12:49:26 +0200

    docs: Fix docu for DistributionFunctions
    (cherry picked from commit 1c9b8d4e4858e12c1b4bcde13a7292053849dbd5)

--- Documentation/004_development/distribution_functions.rst
+++ Documentation/004_development/distribution_functions.rst
@@ -2,15 +2,8 @@
 
 Distribution functions
 ======================
-
+	
 .. doxygenfile:: DistributionFunctions.h
 	:project: dyssol_models_api
-..    :members:
-..    :protected-members:
-..    :private-members:
-..    :members-only:
-..    :outline:
-..    :no-link:
-..    :allow-dot-graphs:
 
 |
--- ModelsAPI/DistributionFunctions.h
+++ ModelsAPI/DistributionFunctions.h
@@ -19,7 +19,9 @@ enum class EDistributionFunction : uint8_t
 	GGS = 3,		///< Gates-Gaudin-Schuhmann distribution.
 	LOG_NORMAL = 4,	///< Log-normal distribution.
 
+	/// \cond <- to hide in the Doxygen output
 	COUNT_	        ///< Number of distribution functions.
+	/// \endcond
 };
 
 namespace details
@@ -41,36 +43,47 @@ namespace details
 
 /**
  * Returns the given probability density function.
- *
- * \details
- * For EDistributionFunction::NORMAL:
- *   \f$y(x) = \frac{1}{\sqrt{2\pi\sigma^{2}}}e^{-\frac{(x-\mu)^{2}}{2\sigma^{2}}}\f$, with \f$\sigma \neq 0\f; returns empty vector otherwise.
- *
- * For EDistributionFunction::LOGNORMAL:
- *   \f$y(x) = \frac{1}{x\sigma\sqrt{2\pi}}e^{-\frac{(\ln x-\mu)^{2}}{2\sigma^{2}}}\f,
- *   with \f$\sigma > 0\f and \f$x > 0\f; returns empty vector otherwise.
- *
- * For EDistributionFunction::RRSB:
- *   \f$y(x) = \frac{k}{\lambda}\left(\frac{x}{\lambda}\right)^{k-1}e^{-\left(\frac{x}{\lambda}\right)^{k}}\f,
- *   with \f$\lambda \neq 0\f, \f$k > 0\f and \f$x > 0\f; returns empty vector otherwise.
- *
- * For EDistributionFunction::GGS:
- *   \f$y(x) = \frac{m}{x_{max}}\left(\frac{x}{x_{max}}\right)^{m-1}\f,
- *   with \f$x_{max} > 0\f, \f$m > 0\f and \f$0 \le x \le x_{max}\f; returns empty vector otherwise.
- *
- * For EDistributionFunction::MANUAL:
+ * \details Available functions:
+ * - EDistributionFunction::NORMAL:
+ *   \f$y(x) = \frac{1}{\sqrt{2\pi\sigma^{2}}}e^{-\frac{(x-\mu)^{2}}{2\sigma^{2}}}\f$,
+ *   
+ *   with \f$\sigma \neq 0\f$,
+ *   \f$\mu\f$ - mean value,
+ *   \f$\sigma\f$ - standard deviation.
+ * - EDistributionFunction::LOG_NORMAL:
+ *   \f$y(x) = \frac{1}{x\sigma\sqrt{2\pi}}e^{-\frac{(\ln x-\mu)^{2}}{2\sigma^{2}}}\f$,
+ *   
+ *   with \f$\sigma > 0\f$ and \f$x > 0\f$,
+ *   \f$\mu\f$ - mean value,
+ *   \f$\sigma\f$ - standard deviation.
+ * - EDistributionFunction::RRSB:
+ *   \f$y(x) = \frac{k}{\lambda}\left(\frac{x}{\lambda}\right)^{k-1}e^{-\left(\frac{x}{\lambda}\right)^{k}}\f$,
+ *   
+ *   with \f$\lambda \neq 0\f$, \f$k > 0\f$ and \f$x > 0\f$,
+ *   \f$\lambda\f$ - characteristic size,
+ *   \f$k\f$ - distribution modulus.
+ * - EDistributionFunction::GGS:
+ *   \f$y(x) = \frac{m}{x_{max}}\left(\frac{x}{x_{max}}\right)^{m-1}\f$,
+ *   
+ *   with \f$x_{max} > 0\f$, \f$m > 0\f$ and \f$0 \le x \le x_{max}\f$,
+ *   \f$x_{max}\f$ - maximum size,
+ *   \f$m\f$ - distribution modulus.
+ * - EDistributionFunction::MANUAL:
  *   returns a vector of zeroes.
  *
- * \param _type  Type of the distribution function.
- * \param _x     Points for which the distribution has to be generated.
+ * Returns an empty vector if the constraints on function parameters are not satisfied.
+ * \param _type    Type of the distribution function.
+ * \param _x       Points for which the distribution has to be generated.
  * \param _param1  First parameter of the distribution function:
- *                 - NORMAL, LOGNORMAL: mean (\f$\mu\f; any real)
- *                 - RRSB: characteristic size (\f$\lambda\f)
- *                 - GGS: maximum size (\f$x_{max}\f)
+ *                 - EDistributionFunction::NORMAL: mean \f$\mu\f$
+ *                 - EDistributionFunction::LOG_NORMAL: mean \f$\mu\f$
+ *                 - EDistributionFunction::RRSB: characteristic size \f$\lambda\f$
+ *                 - EDistributionFunction::GGS: maximum size \f$x_{max}\f$
  * \param _param2  Second parameter of the distribution function:
- *                 - NORMAL, LOGNORMAL: standard deviation (\f$\sigma\f)
- *                 - RRSB: distribution modulus (\f$k\f)
- *                 - GGS: distribution modulus (\f$m\f)
+ *                 - EDistributionFunction::NORMAL: standard deviation \f$\sigma\f$
+ *                 - EDistributionFunction::LOG_NORMAL: standard deviation \f$\sigma\f$
+ *                 - EDistributionFunction::RRSB: distribution modulus \f$k\f$
+ *                 - EDistributionFunction::GGS: distribution modulus \f$m\f$
  * \return         Distribution.
  */
 std::vector<double> CreateDistribution(EDistributionFunction _type, const std::vector<double>& _x, double _param1, double _param2);
