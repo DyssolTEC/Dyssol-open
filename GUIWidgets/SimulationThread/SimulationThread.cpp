db668b0494a14a61354ca47f3d3571b95c78b5a6
Vasyl Skorych <vasyl.skorych@gmail.com>
2025-05-08 15:15:00 +0200

    refactor: Minor refactoring in Qt classes
    (cherry picked from commit 62a6ea7a5473bf7a4691ec9513ebd60b00f21bff)

--- GUIWidgets/SimulationThread/SimulationThread.cpp
+++ GUIWidgets/SimulationThread/SimulationThread.cpp
@@ -3,8 +3,19 @@
 
 #include "SimulationThread.h"
 
-CSimulationThread::CSimulationThread(std::function<void()> _run, std::function<void()> _stop, QObject* _parent) :
-	CBasicThread(_parent)
+CSimulationThread::CSimulationThread(const RunFunc& _run, const StopFunc& _stop, QObject* _parent)
+	: CBasicThread{ _parent }
+	, m_run{ _run }
+	, m_stop{ _stop }
+{
+}
+
+CSimulationThread::CSimulationThread(QObject* _parent)
+	: CSimulationThread{ RunFunc{}, StopFunc{}, _parent }
+{
+}
+
+void CSimulationThread::SetFunctions(const RunFunc& _run, const StopFunc& _stop)
 {
 	m_run = _run;
 	m_stop = _stop;
@@ -18,12 +29,17 @@ bool CSimulationThread::WasAborted() const
 void CSimulationThread::StartTask()
 {
 	m_aborted = false;	// reset flag
-	m_run();
+
+	if (m_run)
+		m_run();
+
 	emit Finished();
 }
 
 void CSimulationThread::RequestStop()
 {
-	m_stop();
+	if (m_stop)
+		m_stop();
+
 	m_aborted = true;
 }
